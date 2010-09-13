98a62a6526e56fe621efc6e5b8f19e6525fc8419
Preston Skupinski <skupinsk@cse.msu.edu>
2010-09-12 23:45:14 -0400

    intial commit

---
+++ src/com/difficultology/flargmud/Main.java
@@ -0,0 +1,11 @@
+package com.difficultology.flargmud;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("In main!");
+  }
+  
+  public static boolean getTrue() {
+    return true;
+  }
+}
---
+++ tests/com/difficultology/flargmud/MainTest.java
@@ -0,0 +1,8 @@
+package com.difficultology.flargmud;
+import junit.framework.*;
+
+public class MainTest extends TestCase {
+  public void testMainGetTrue() {
+    assertTrue(Main.getTrue());
+  }
+}
